2e86304cd5ddd4fc0ed13768c4fd16a83c44005a
Jailson Xavier <xavier.jailson@outlook.com>
2020-10-23 21:13:17 -0300

    App-Mata-Mosquito

---
+++ jogo.js
@@ -0,0 +1,128 @@
+var altura = 0;
+var largura = 0;
+var vidas = 1;
+var tempo = 15;
+
+var tempoMosquito = 1500;
+
+var nivel = window.location.search
+nivel = nivel.replace('?','');
+//alert(nivel.replace('?',''));
+if(nivel === "normal"){
+	//1500
+	tempoMosquito = 1500;
+} 
+else if(nivel === "dificil"){
+	//1000
+	tempoMosquito = 1000;
+
+}else if(nivel === "chuknorris"){
+	//750
+	tempoMosquito = 750;
+
+}
+
+function ajustatamanhodopainel() {
+	altura = window.innerHeight
+	largura =window.innerWidth
+
+	console.log(altura,largura);
+
+	}
+
+	ajustatamanhodopainel();
+
+	var cronometro = setInterval(function(){
+		tempo-=1;
+		if (tempo < 0) {
+			clearInterval(cronometro);
+			clearInterval(mosca);	
+			window.location.href='vitoria_do_jogo.html';		
+		}else{
+			document.getElementById('cronometro').innerHTML = tempo
+		}
+	},1000)
+
+	function posicaoRandomica(){
+
+		//Lógica que remove o mosquito após selecionar
+		if (document.getElementById('mosquito')){ 		
+			document.getElementById('mosquito').remove();
+			if(vidas > 3){
+				window.location.href = "fim_do_jogo.html"
+
+			} else{
+			document.getElementById('v' + vidas).src='imagens/coracao_vazio.png'
+			vidas ++;
+			}
+		} 
+
+	var posicaox = Math.floor(Math.random() * largura) -90;
+	var posicaoy = Math.floor(Math.random() * altura) -90;
+
+	posicaox = posicaox < 0 ? 0: posicaox
+	posicaoy = posicaoy < 0 ? 0: posicaoy
+
+	console.log(posicaox, posicaoy);
+
+	//Criar o elemento html
+	var mosquito = document.createElement('img');
+	mosquito.src = 'imagens/mosquito.png';
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
+	mosquito.style.left = posicaox + 'px';
+	mosquito.style.top = posicaoy + 'px';
+	mosquito.style.position = 'absolute';
+	mosquito.id ='mosquito';
+	mosquito.onclick = function(){
+		this.remove();		
+	}
+
+	
+
+	document.body.appendChild(mosquito);
+
+	console.log(tamanhoAleatorio());
+
+	console.log(ladoAleatorio());
+}
+
+function tamanhoAleatorio(){
+	var classe = Math.floor(Math.random() * 3);
+
+	switch(classe){
+		case 0:
+		return 'mosquito1';
+
+		case 1:
+		return 'mosquito2';
+
+		case 2:
+		return 'mosquito3';
+
+	}
+	console.log(classe); 
+}
+
+function ladoAleatorio(){
+	var classe = Math.floor(Math.random() * 2);
+
+	switch(classe){
+		case 0:
+		return 'ladoA'
+
+		case 1:
+		return 'ladoB'
+	}
+
+
+}
+
+function iniciarJogo(){
+				var nivel = document.getElementById('nivel').value
+				if(nivel == "") {
+				alert('Selecione o nível para iniciar');
+				return false
+				} else{
+				window.location.href='app1.html?' + nivel;
+				}
+			}//Fim função inciar Jogo
